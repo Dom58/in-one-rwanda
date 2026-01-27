e35f083c7c779d1a7025ccdb9d44c33796565cc7
Dom58 <dndahimana58@gmail.com>
2026-01-27 15:40:16 +0200

    Adding Hikr CDN tracker

--- src/components/common/HikrTrackingScript.tsx
+++ src/components/common/HikrTrackingScript.tsx
@@ -17,21 +17,22 @@ const HikrTrackingScript: FC<HikrTrackingScriptProps> = ({
   return (
     <>
       <Script
-        //src="http://localhost:9000/script.js" //replace with the actual path to the CDN script where deployed
-        //E.g: https://static.staticsave.com/hikrcdn/script.js or /script.js from your public folder
+        src="http://localhost:9000/script.js" 
+        //E.g: https://static.staticsave.com/hikrcdn/script.js or DEployed CDN link
         strategy="lazyOnload"
         onLoad={() => {
-          window.hikr.config({ websiteId: websiteId, userConsent: true })
-            .then(() => console.log('Configuration successful'))
-            .catch((error) => console.error('Configuration failed', error));
+          if (window.hikr) {
+            window.hikr.config({ websiteId: websiteId, userConsent: true })
+              .then(() => console.log('Configuration successful'))
+              .catch((error) => console.error('Configuration failed', error));
 
-          window.hikr.pushData({
-            eventType: "click",
-            collectionName: "userInteractions",
-            data: "",
-            userConsent: true
-          });
-
+            window.hikr.pushData({
+              eventType: "click",
+              collectionName: "userInteractions",
+              data: "",
+              userConsent: true
+            });
+          }
         }}
         onError={(e) => {
           console.error('Script failed to load:', e);
